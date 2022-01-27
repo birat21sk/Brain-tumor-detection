21f223a06157c4c22b05718371d4e606057690d8
birat21sk <birat.sk21@gmail.com>
2022-01-28 00:51:19 +0545

    Config App

--- _config.py
+++ _config.py
@@ -1,3 +1,18 @@
+from tkinter import Tk
+
+class Iconified(Tk):    
+    def __init__(self):
+        Tk.__init__(self)
+        self.attributes('-alpha', 0.0)
+
+    
+
+# iconifiedme = NewRoot()
+# iconifiedme.lower()
+# iconifiedme.iconify()
+# iconifiedme.title('Spam 2.0')
+
+
 
 class Window():
     def __init__(self,root):
