4926f872812114125d10623890024ef4a8ad1452
birat21sk <birat.sk21@gmail.com>
2022-01-28 23:22:58 +0545

    v1.1

--- TumorApplication.py
+++ TumorApplication.py
@@ -1,5 +1,6 @@
 from ctypes import windll
 from pathlib import Path
+import time
 from tkinter import (
     Tk, 
     Canvas,
@@ -9,14 +10,18 @@ from tkinter import (
     PhotoImage, 
     filedialog,
     messagebox as mb
-)  
+)
 from PIL import Image, ImageTk
 
 from exception import FileTypeException
-from prediction import predict_tumor
+# from prediction import predict_tumor
 from preprocess import is_jpg, preprocess
+from splash_screen import SplashScreen
 from _global import * 
+from _config import place_center
 
+WIN_WIDTH=750
+WIN_HEIGHT=531
 #for taskbar integration
 GWL_EXSTYLE = -20
 WS_EX_APPWINDOW = 0x00040000
@@ -25,62 +30,80 @@ WS_EX_TOOLWINDOW = 0x00000080
 class TumorAppication(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.geometry("750x531+150+150") 
+        x_cord, y_cord = place_center(self,WIN_WIDTH,WIN_HEIGHT)
+        self.geometry("%dx%d+%d+%d"%(WIN_WIDTH,WIN_HEIGHT,x_cord,y_cord)) 
         self.configure(bg = "#ffffff")
-        self.title("Brain Tumor Detection")
+        self.title("Cilios")
+        self.iconbitmap('./icon.ico')
         self.resizable(False, False)
         self.overrideredirect(True) #hides title bar
         self.bind("<Map>", self.canvas_mapped)
-        self.after(500, lambda: self.set_taskbar(self))
-        self.z=0
-
+        self.after(10, lambda: self.set_taskbar(self))
+        self.min = 0
 
     def set_taskbar(self,master): 
-        hwnd = windll.user32.GetParent(master.winfo_id())
+        hwnd = windll.user32.GetParent(master.winfo_id())  #Process ID (Handle Window)
         stylew = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
         stylew = stylew & ~WS_EX_TOOLWINDOW
         stylew = stylew | WS_EX_APPWINDOW
-        res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, stylew)
+        res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, stylew) #Sets a new extended window style.
         # re-assert the new window style
         master.wm_withdraw()
         master.after(10, lambda: master.wm_deiconify())
 
+    def canvas_mapped(self,e): 
+            self.overrideredirect(True)
+            if self.min == 1:
+                self.set_taskbar(self)
+                self.min = 0
+                
     def minimize(self):
         self.state('withdrawn')
         self.overrideredirect(False)
         self.state('iconic')
-        self.z = 1
+        self.min = 1
 
-    def canvas_mapped(self,e): 
-        self.overrideredirect(True)   
-
-        if self.z == 1:
-            self.set_taskbar(self)
-            self.z = 0
-    
     def move_app(self,e): 
         self.geometry(f'+{e.x_root - 672}+{e.y_root - 20}')
 
-    def display_image(self,input_img):
+    def display_image(self,image):
         #========== Update UI ==========#
-        input_img = input_img.resize((284,284))
-        img_tk = ImageTk.PhotoImage(input_img) 
-        self.show_img=Label(self.canvas,image = img_tk,bd=0)
-        self.show_img.image = img_tk
-        self.show_img.place(x=70,y=101)
+        image = image.resize((284,284))
+        img_tk = ImageTk.PhotoImage(image)  
+        self.show_result_img=Label(self.canvas,image = img_tk,bd=0)
+        self.show_result_img.image = img_tk
+        self.show_result_img.place(x=60,y=101)
         #========== Update UI ==========#
+    
+    def clear_result(self): 
+        try:
+            self.show_result_img.destroy()
+            self.canvas.itemconfig(self.pred_result, text="")
+            self.canvas.itemconfig(self.pred_result_summary, text="")
+        except AttributeError as ae:
+            print(ae)
+
+    def reset_screen(self):
+        try:
+            self.show_img.destroy()
+            self.show_result_img.destroy()
+            self.path_entry.delete(0, END)
+            self.path_entry.insert(0, "Choose file (only jpg)...")
+            self.canvas.itemconfig(self.pred_result, text="")
+            self.canvas.itemconfig(self.pred_result_summary, text="")
+            self.input_img_path=None
+        except AttributeError as ae:
+            print(ae)
 
     def detect_tumor(self):
-        try:        
-            self.img_is_jpg = is_jpg(self.input_img_path)
-            
+        try:
+            self.img_is_jpg = is_jpg(self.input_img_path)            
             if self.img_is_jpg:
                 self.img_to_pred = preprocess(self.input_img_path)
-                
-                self.result = predict_tumor(self.img_to_pred)
-                self.result = self.result.flatten()
-                self.result = round(self.result[0],4)
-
+                # self.result = predict_tumor(self.img_to_pred)
+                # self.result = self.result.flatten()
+                # self.result = round(self.result[0],4)
+                self.result = 0.99985
                 if self.result > 0.5:
                     self.canvas.itemconfig(self.pred_result, text="Tumor detected ")
                     self.canvas.itemconfig(self.pred_result_summary, text="Tumor was detected in the MRI image with with "+str((self.result*100))+"% confidence")
@@ -88,48 +111,48 @@ class TumorAppication(Tk):
                     self.canvas.itemconfig(self.pred_result, text="Tumor not detected")
                     self.canvas.itemconfig(self.pred_result_summary, text="Tumor was Not detected in the MRI image with with "+str((self.result*100))+"% confidence")
                 
-            self.display_image(self.input_img)           
-
-            print("hey")
-            # else:
-            #     raise FileTypeException
+                self.display_image(self.input_img)
+            else:
+                raise FileTypeException
         
         except FileTypeException as fe:
-            print("Error: File type unknown")
+            print(fe)
             mb.showerror("Unknown file type","Error: File Type Unknown, select jpg image")
 
-        except Exception as e:
-            # print("\nSelect the MRI image first")
-            print(e)
-            mb.showinfo("No image selected","Please select the MRI image first")
+        except NameError as ne:
+            print(ne)
+            mb.showerror("Error","Unable to upload image")
 
-    def get_image(self,e):
-        # global input_img_path
-        # global input_img
+        except AttributeError as ae:
+            print(ae)
+            mb.showwarning("No image selected","Please select the MRI image first")
+            
+        except Exception as e:
+            print(e)
+            mb.showerror("Error","Unexpected Error Occoured! Please try again")
+
+    def get_image(self,event): 
         try:
             self.input_img_path = filedialog.askopenfilename()
             self.img_is_jpg = is_jpg(self.input_img_path)
             if self.img_is_jpg:
                 self.path_entry.delete(0, END)
-                self.path_entry.insert(0, self.input_img_path)
-                
+                self.path_entry.insert(0, self.input_img_path) 
+                self.loading_image =  Image.open(self.relative_to_assets('image_1.png'))
                 self.input_img = Image.open(self.input_img_path)
                 self.resized_input_img = self.input_img.resize((208,208))
-
-                self.input_image_tk = ImageTk.PhotoImage(self.resized_input_img) 
-                
+                self.input_image_tk = ImageTk.PhotoImage(self.resized_input_img)                 
                 self.show_img=Label(self.canvas,image = self.input_image_tk,bd=0)
                 self.show_img.image = self.input_image_tk
-                self.show_img.place(x=463,y=136)
+                self.show_img.place(x=440,y=123)
             else:
                 raise FileTypeException
 
-        except FileTypeException:
-            print("Error: File type unknown")
+        except FileTypeException as fe: 
+            print(fe)
             mb.showerror("Unknown file type","Error: File Type Unknown, select jpg image")
-
-        except AttributeError:
-            print("Please select the file")
+        except AttributeError as ae:
+            print(ae)
             mb.showerror("No file selected","No file was selected")
 
     def custom_title_bar(self): 
@@ -138,9 +161,10 @@ class TumorAppication(Tk):
             17.0,
             14.91,
             anchor="nw",
-            text="Brain Tumor Detection",
+            text="Cilios - Brain Tumor Detection",
             fill="#FFFFFF",
-            font=("OpenSansRoman-Regular",10)
+            # font=("OpenSansRoman-Regular",10)
+            font=("Montserrat Regular",10)
         )
         #===============Title===============#
 
@@ -150,8 +174,8 @@ class TumorAppication(Tk):
             image=self.drag_button,
             borderwidth=0,
             highlightthickness=0,
-            bg="#51848F",
-            activebackground='#51848E', 
+            bg="#4f9e9b",
+            activebackground='#4d9996', 
             relief="flat",
             cursor="diamond_cross"
         )
@@ -169,8 +193,8 @@ class TumorAppication(Tk):
         self.button_4 = Button(
             image=self.min_button,
             borderwidth=0,
-            bg="#51848F",
-            activebackground='#51848E',
+            bg="#4f9e9b",
+            activebackground='#4d9996',
             highlightthickness=0,
             command=self.minimize,
             relief="flat",
@@ -190,9 +214,9 @@ class TumorAppication(Tk):
             image=self.close_button,
             borderwidth=0,
             highlightthickness=0,
-            bg="#51848F",
-            activebackground='#51848E',
-            command=self.canvas.quit,
+            bg="#4f9e9b",
+            activebackground='#4d9996',
+            command=self.destroy,
             relief="flat",
             cursor="hand2"
         )
@@ -207,54 +231,56 @@ class TumorAppication(Tk):
     def input_block(self):
         #====================Input Block====================#
         self.canvas.create_text(
-            511.89,
-            95.91,
+            469.0,
+            86.0,
             anchor="nw",
             text="Image Upload",
             fill="#FFFFFF",
-            font=("OpenSansRoman-Regular",int(16.62))
-        ) 
+            font=("Montserrat Regular", int(16.62))
+            # font=("OpenSansRoman-Regular", int(16.62))
+        )
         self.img = PhotoImage(file=self.relative_to_assets('image_1.png'))
-        self.canvas.create_image(463,136, anchor='nw', image=self.img)
+        self.canvas.create_image(440,123, anchor='nw', image=self.img)
         self.imgs = PhotoImage(file=self.relative_to_assets('entry_1.png'))
-        self.canvas.create_image(463,365, anchor='nw', image=self.imgs)
+        self.canvas.create_image(440,345, anchor='nw', image=self.imgs)
         
         self.path_entry = Entry(
             bd = 0,
             bg = "#ffffff",
+            foreground="#171717",
             highlightthickness = 0,
-            font=("OpenSansRoman-Regular",int(10)))
+            # font=("OpenSansRoman-Regular",int(10)))
+            font=("Montserrat Regular",int(10)))
         self.path_entry.insert(0,'Choose file (only jpg)...')
         self.path_entry.place(
-            x=467.0,
-            y=364.91,
-            width=200.0,
+            x=450.0,
+            y=345.91,
+            width=190.0,
             height=26.0
         )
         self.path_entry.bind("<1>", self.get_image)
 
         #===============Browse Button===============#
-        self.browse_btn_img = PhotoImage(file=self.relative_to_assets("button_1b.png"))
+        self.browse_btn_img = PhotoImage(file=self.relative_to_assets("button_browse.png"))
         self.browse_btn = Button(
             image=self.browse_btn_img,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: print("Browse clicked"),
             relief="flat",
             cursor="hand2"
         )
         self.browse_btn.place(
-            x=463.0,
-            y=415.0,
-            width=92.5,
+            x=440.0,
+            y=387.0,
+            width=93.0,
             height=36.36
-        ) 
+        )
         self.browse_btn.bind("<1>", self.get_image)
         #===============Browse Button===============#
 
 
         #===============Detect Button===============#
-        self.detect_btn_img = PhotoImage(file=self.relative_to_assets("button_2d.png"))
+        self.detect_btn_img = PhotoImage(file=self.relative_to_assets("button_detect.png"))
         self.detect_btn = Button(
             image=self.detect_btn_img,
             borderwidth=0,
@@ -264,31 +290,66 @@ class TumorAppication(Tk):
             cursor="hand2"
         )
         self.detect_btn.place(
-            x=577.0,
-            y=415.0,
-            width=92.5,
+            x=555.0,
+            y=387.0,
+            width=93.0,
             height=36.36
         ) 
         #===============Detect Button===============#
+        #===============Clear Button===============#
+        self.clear_btn_img = PhotoImage(file=self.relative_to_assets("button_clear.png"))
+        self.clear_btn = Button(
+            image=self.clear_btn_img,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.clear_result,
+            relief="flat",
+            cursor="hand2"
+        )
+        self.clear_btn.place(
+            x=440.0,
+            y=437.0,
+            width=93.0,
+            height=36.36
+        ) 
+        #===============Clear Button===============#
+
+        #===============Reset Button===============#
+        self.reset_btn_img = PhotoImage(file=self.relative_to_assets("button_reset.png"))
+        self.reset_btn = Button(
+            image=self.reset_btn_img,
+            borderwidth=0,
+            highlightthickness=0,
+            command = self.reset_screen,
+            relief="flat",
+            cursor="hand2"
+        )
+        self.reset_btn.place(
+            x=555.0,
+            y=437.0,
+            width=93.0,
+            height=36.36
+        ) 
+        #===============Reset Button===============#
         #====================Input Block====================#
 
     def result_block(self):
         #===============Result Block===============#
         self.pred_result = self.canvas.create_text(
-            70.0,
+            60.0,
             400.0,
             width=290,
             anchor="nw", 
             fill="#FFFFFF",
-            font=("OpenSansRoman-Regular",int(16.62))
+            font=("Montserrat Medium",int(16.62))
         )
         self.pred_result_summary = self.canvas.create_text(
-            70.0,
+            60.0,
             435.91,
             width=290,
             anchor="nw", 
             fill="#FFFFFF",
-            font=("OpenSansRoman-Regular",10)
+            font=("Montserrat Regular",10)
         )
         #===============Result Block===============#
 
@@ -309,27 +370,16 @@ class TumorAppication(Tk):
 
     def relative_to_assets(self,path: str) -> Path:
         return ASSETS_PATH / Path(path)
- 
-def main(): 
-    # destroy splash window
-    splash_root.destroy()
-    
+     
+
+def main():
+    splash.destroy()
     app = TumorAppication()
     app.create_ui()
     app.mainloop()
 
-
-if __name__ == '__main__':
-    splash_root = Tk()
-    
-    # Adjust size
-    splash_root.geometry("200x200")
-    
-    # Set Label
-    splash_label = Label(splash_root,text="Splash Screen",font=18)
-    splash_label.pack()
-    
-    splash_root.after(3000,main)
-    splash_root.mainloop()
-
+if __name__ == '__main__': 
+    splash = SplashScreen()
+    splash.after(3000, main)
+    splash.mainloop()
     
