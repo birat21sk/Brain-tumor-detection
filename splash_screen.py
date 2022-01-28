4d923e824c69618ec574e579f6a9b93f5516f2f4
birat21sk <birat.sk21@gmail.com>
2022-01-28 23:40:17 +0545

    Splash main

--- splash_screen.py
+++ splash_screen.py
@@ -1,26 +1,23 @@
 from tkinter import *
 from PIL import Image,ImageTk
+from _config import place_center
 
 class SplashScreen(Tk):
     def __init__(self,master=None):
         Tk.__init__(self)
-        # self.master.attributes("-alpha",0.0)
         self.overrideredirect(True)
         self.alpha = self.attributes("-alpha")
          
         splash_width = 427
         splash_height = 250
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x_cord = (screen_width/2)-(splash_width/2)
-        y_cord = (screen_height/2)-(splash_height/2)
-        self.geometry("%dx%d+%d+%d"%(splash_width,splash_height,x_cord,y_cord))
-        self.after(2000, self.fade_out) 
 
-        self.fm =Frame(self,width=splash_width,height=splash_height, bg="#51848E").place(x=splash_width/2,y=splash_height/2,anchor='center')
-        # self.fm.pack()
-        # self.fm.place(anchor='center', relx=0.5, rely=0.5)
-        self.img = ImageTk.PhotoImage(Image.open("image_1.png"))
+        x_cord, y_cord = place_center(self,splash_width,splash_height)
+
+        self.geometry("%dx%d+%d+%d"%(splash_width,splash_height,x_cord,y_cord))
+        self.after(2500, self.fade_out) 
+ 
+        self.fm =Frame(self,width=splash_width,height=splash_height, bg="#ffffff",bd=0).place(x=0,y=0,anchor='nw')
+        self.img = ImageTk.PhotoImage(Image.open("./assets/splash.png"))
         self.label = Label(self.fm, image = self.img)
         self.label.place(anchor='center', relx=0.5, rely=0.5)
 
@@ -29,9 +26,5 @@ class SplashScreen(Tk):
             self.alpha -= .1
             self.wm_attributes("-alpha",self.alpha)
             self.after(30, self.fade_out)
-        if self.alpha <= 0:
-            self.destroy()
-
-
-s= SplashScreen()
-s.mainloop()
+        # if self.alpha <= 0:
+        #     self.destroy()
