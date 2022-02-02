a04ff2e185cb64306cff18632a3781c87aabdb61
birat21sk <birat.sk21@gmail.com>
2022-02-03 01:32:07 +0545

    K-Means integration

--- threshold.py
+++ threshold.py
@@ -1,16 +1,24 @@
 import numpy as np
 import cv2  
 
-def threshold(image_path):    
+def image_segmentation(image_path):    
     '''
+    Parameters:
         image_path: Path to image
     '''
     inp_img = cv2.imread(image_path)
     img = inp_img.reshape((-1,3))
     img = np.float32(img)
+    k=4 # Number of centroids
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    k=4
-    ret,label,center=cv2.kmeans(img,k,None,criteria,10,cv2.KMEANS_PP_CENTERS)
+    '''
+        TERM_CRITERIA_EPS - stop the algorithm iteration if specified accuracy, epsilon, is reached.
+        TERM_CRITERIA_MAX_ITER - stop the algorithm after the specified number of iterations, max_iter.
+        TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER - stop the iteration when any of the above condition is met.
+    '''
+    attempts = 10 # Flag to specify the number of times the algorithm is executed using different initial labellings.
+    # K-Means with KMeans++ initial center
+    ret,label,center=cv2.kmeans(img,k,bestLabels=None,criteria=criteria,attempts=attempts,flags=cv2.KMEANS_PP_CENTERS)
     center = np.uint8(center)
     result = center[label.flatten()]
     bright = np.amax(center)
