ec11cc0d90087979a2e4308109c3260b1db49177
birat21sk <birat.sk21@gmail.com>
2022-02-28 14:44:07 +0545

    Update Prediction Model

--- prediction.py
+++ prediction.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-pred_model = tf.keras.models.load_model("./model/TumorModel10Epochs.h5")
+pred_model = tf.keras.models.load_model("./model/BrainTumorModel-10FoldValidation.h5")
 def predict_tumor(image):
     result = pred_model.predict(image)
     return result
